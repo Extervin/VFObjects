9cb345a89a0e8693df5aa9c8dfd72d48c43b906c
drouz <alexo.prokofiev@gmail.com>
2024-01-30 11:25:07 +0200

    Added transliteration (Latin to Cyrillic) for search function

--- assets/js/searchWorker.js
+++ assets/js/searchWorker.js
@@ -8,12 +8,56 @@ onmessage = function(e) {
         // Инициализируем Fuse при получении сообщения 'initialize'
         fuse = new Fuse(e.data.data, {
             keys: ["Obekt", "IT"],
-            threshold: 0.4
+            threshold: 0.4,
+            includeMatches: true, // Включаем поиск по кириллице
+            ignoreLocation: true // Игнорируем местоположение для точного совпадения
         });
     } else if (e.data.action === 'search') {
-        // Выполняем поиск при получении сообщения 'search'
+        // Выполняем транслитерацию текста, если он введен на латинице
         var filter = e.data.filter;
-        var searchResult = fuse.search(filter);
+        var transliteratedFilter = transliterateLatinToCyrillic(filter);
+
+        // Выполняем поиск с использованием транслитерированного фильтра
+        var searchResult = fuse.search(transliteratedFilter);
+
         postMessage(searchResult);
     }
 };
+
+// Функция для транслитерации текста с латиницы на кириллицу
+function transliterateLatinToCyrillic(text) {
+    var transliterationMap = {
+        'a': 'а',
+        'b': 'б',
+        'c': 'ц',
+        'd': 'д',
+        'e': 'е',
+        'f': 'ф',
+        'g': 'г',
+        'h': 'х',
+        'i': 'и',
+        'j': 'ж',
+        'k': 'к',
+        'l': 'л',
+        'm': 'м',
+        'n': 'н',
+        'o': 'о',
+        'p': 'п',
+        'q': 'кю',
+        'r': 'р',
+        's': 'с',
+        't': 'т',
+        'u': 'у',
+        'v': 'в',
+        'w': 'в',
+        'x': 'кс',
+        'y': 'й',
+        'z': 'з',
+        // Добавьте остальные соответствия по необходимости
+    };
+
+    // Проходим по каждой букве и выполняем замену
+    return text.toLowerCase().split('').map(function(char) {
+        return transliterationMap[char] || char;
+    }).join('');
+}
